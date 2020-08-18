05d611bb8787d97c45b25926e0da1fb01d7c4d53
gteoharov <gt@appfactory.com>
2020-08-18 16:52:34 +0300

    Custom transition and full screen size

--- DesignCode/DesignCode/CourseList.swift
+++ DesignCode/DesignCode/CourseList.swift
@@ -11,11 +11,30 @@ import SwiftUI
 struct CourseList: View {
     var body: some View {
         VStack {
+            CourseView()
+        }
+        
+    }
+}
+
+struct CourseList_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseList()
+    }
+}
+
+struct CourseView: View {
+    @State var show = false
+    
+    var body: some View {
+        VStack {
             HStack(alignment: .top) {
                 VStack(alignment: .leading, spacing: 8.0) {
                     Text("SwiftUI Advanced")
                         .font(.system(size: 24, weight: .bold))
+                        .foregroundColor(.white)
                     Text("20 sections")
+                        .foregroundColor(Color.white.opacity(0.8))
                 }
                 Image(uiImage: #imageLiteral(resourceName: "Logo1"))
             }
@@ -28,17 +47,16 @@ struct CourseList: View {
                 .frame(maxWidth: .infinity)
                 .frame(height: 140, alignment: .top)
         }
-        .padding(20)
-        .frame(width: screen.width - 60, height: 280)
+        .padding(show ? 30 : 20)
+        .padding(.top, show ? 30 : 0)
+        .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? .infinity : screen.height / 3)
         .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
         .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
         .shadow(color: Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)).opacity(0.3), radius: 20, x: 0, y: 20)
-        
-    }
-}
-
-struct CourseList_Previews: PreviewProvider {
-    static var previews: some View {
-        CourseList()
+        .animation(.spring(response: 0.4, dampingFraction: 0.6, blendDuration: 0))
+        .onTapGesture {
+            self.show.toggle()
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
